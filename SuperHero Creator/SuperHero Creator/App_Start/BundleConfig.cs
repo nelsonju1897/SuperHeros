ab864fe8627975edfc2a7d4ed1fae61feb3c7698
nelsonju1897 <nelsonju1897@gmail.com>
2019-11-22 09:39:51 -0600

    Completed changing the bootstrap and adding image. Project is MVP. Will be working to improve project.

--- SuperHero Creator/SuperHero Creator/App_Start/BundleConfig.cs
+++ SuperHero Creator/SuperHero Creator/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace SuperHero_Creator
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-cyborg.css",
                       "~/Content/site.css"));
         }
     }
--- SuperHero Creator/SuperHero Creator/Models/SuperHero.cs
+++ SuperHero Creator/SuperHero Creator/Models/SuperHero.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,10 +11,20 @@ namespace SuperHero_Creator.Models
     {
         [Key]
         public  int Id { get; set; }
+
+        [DisplayName ("SuperHero Name")]
         public string superHeroName { get; set; }
+
+        [DisplayName("Alter Ego")]
         public string alterEgo { get; set; }
+
+        [DisplayName("Primary Ability")]
         public string primaryAbility { get; set; }
+
+        [DisplayName("Secondary Ability")]
         public string secondaryAbility { get; set; }
+
+        [DisplayName("Catchphrase")]
         public string catchphrase { get; set; }
 
     }
